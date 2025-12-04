1374129b3d0c3322e2e0e6a515732c62192051f2
JarnotMaciej <maciekjarnot@gmail.com>
2025-12-04 22:31:10 +0100

    refactor: remove unused output and update resource group reference in workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "aks_resource_group" {
-  value = azurerm_resource_group.aks_cluster.resource_group_name
-}
-
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks_cluster.name
 }
